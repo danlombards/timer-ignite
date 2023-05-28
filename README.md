977293c0274addf49eb224f90394300f9fbf2a4d
Danilo Lombardi dos Santos <123340636+danlombards@users.noreply.github.com>
2023-05-27 23:24:58 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+<h1 align="center">
+  Ignite Timer
+</h1>
+
+A React.JS application to countdown desired task time. Fully frontend with React and Vite.
+
+### Home
+<p align="center">
+    <img alt="Ignite Timer home" src="https://user-images.githubusercontent.com/23503499/235727292-8df8be58-89ad-4380-8f0c-f2bd496244a7.png" />
+</p>
+
+### History
+<p align="center">
+    <img alt="Ignite Timer history page" src="https://user-images.githubusercontent.com/23503499/235727639-2fbee141-5156-4f50-872e-26be0cd5188f.png" />
+</p>
+
+
+## :pushpin: Technologies
+- Typescript
+- React.JS
+- Vite
+- NPM
+
+## :pushpin: Libraries
+- styled-components
+- date-fns
+- immer
+- phosphor-react
+- react-hook-form
+- hookform/resolvers
+- zod
+
+## :computer: How to run
+```
+npm install
+
+npm run dev
+
+// It will run on localhost:5173
+```
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,86 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse; // para as bordas não se juntarem ao se encontrar
+    min-width: 600px; // forçar scroll em tamanhos menores:
+
+    th {
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-left: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-left: 1.5rem;
+      }
+    }
+  }
+`
+
+const STATUS_COLOR = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const // isso faz com que o TS entenda que só podem ser passados esses valores (que são os que estão no tema)
+// para evitar erro de TS no comando props.theme[STATUS_COLOR[props.statusColor]]
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLOR
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLOR[props.statusColor]]};
+  }
+`
